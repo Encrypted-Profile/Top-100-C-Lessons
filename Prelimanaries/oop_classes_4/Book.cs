1686f47c5758a321a3afb29ffb8c936dd8c67ced
Elia Haider <baqir.ali2012@gmail.com>
2021-10-20 21:09:29 +0500

    Book

---
+++ Prelimanaries/oop_classes_4/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_classes_4
+{
+    class Book
+    {
+        public int bookId { get; set; }
+        public string bookTitle { get; set; }
+        public string bookAuthor { get; set; }
+
+        public void print(Book b)
+        {
+            Console.WriteLine(b);
+        }
+
+        public override string ToString()
+        {
+            return "Book Title: " + this.bookTitle;
+        }
+
+    }
+}
---
+++ Prelimanaries/oop_classes_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_classes_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book()
+            {
+                bookId = 1,
+                bookTitle = "The sun",
+                bookAuthor = "Mahira"
+            };
+            Book b2 = new Book()
+            {
+                bookId = 2,
+                bookTitle = "The Moon",
+                bookAuthor = "Mahira"
+            };
+
+
+            Book b = new Book();
+            b.print(b1);
+
+            Console.Read();
+        }
+    }
+}
